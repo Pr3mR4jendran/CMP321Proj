81040f39635da882bc67654c72adee2ba66d741d
Pr3mR4jendran <75168548+Pr3mR4jendran@users.noreply.github.com>
2021-12-06 10:40:00 +0400

    Update parser.py

--- parser.py
+++ parser.py
@@ -2,76 +2,78 @@
 
 import re
 
+class ParseError(Exception):
+    def __init__(self, s):
+        print(s) #Prints the specific type of error encountered (text to be passed into the init method of exception class)
 
 def special(c):
    if c in ["+","-","*","/","\\","^","~",":",".","?"," ","#","$","&"]:
        return True
    else :
-       return False
+       raise ParseError("Special character not found")
 
 def digit(c):
     c=re.search("^[0-9]$",c)
     if c:
         return True
     else :
-        return False
+        raise ParseError("Digit not found")
    
 def lowercasechar(c):
     c=re.search("^[a-z]$",c)
     if c:
         return True
     else :
-        return False
+        raise ParseError("Lowercase character not found")
     
 def uppercasechar(c):
     c=re.search("^[A-Z]$",c)
     if c:
         return True
     else :
-        return False
+        raise ParseError("Uppercase character not found")
     
 def numeral(c):
     if digit(c):
         return True
     elif digit(c[0]) and numeral(c[1:]):
         return True
-    return False
+    raise ParseError("Numeral not found")
 
 def alphanumeric(c):
     if lowercasechar(c) or uppercasechar(c) or digit(c):
         return True
-    return False
+    raise ParseError("Alphanumeric character not found")
         
 def character(c):
     if alphanumeric(c) or special(c):
         return True
-    return False
+    raise ParseError("Character not found")
     
 def string(c):
     if character(c):
         return True
     elif character(c[0]) and string(c[1:]):
         return True
-    return False
+    raise ParseError("String not found")
 
 def characterlist(c):
     if alphanumeric(c):
         return True
     elif alphanumeric(c[0]) and characterlist(c[1:]):
         return True
-    return False
+    raise ParseError("Character list not found")
     
 def variable(c):
     if uppercasechar(c):
         return True
     elif uppercasechar(c[0]) and characterlist(c[1:]):
         return True
-    return False
+    raise ParseError("Variable not found")
     
 def smallatom(c):
     if lowercasechar(c):
         return True
     elif lowercasechar(c[0]) and characterlist(c[1:]):
         return True
-    return False
-    
+    raise ParseError("Small atom not found")
